f52a452bc2293d466b144b7f5c14dd3898b8f4f9
gpinchon <gpinchon@student.42.fr>
2021-10-04 23:12:15 +0200

    syntax

--- Driver/OpenGL/src/Image/Image.cpp
+++ Driver/OpenGL/src/Image/Image.cpp
@@ -2,7 +2,7 @@
 * @Author: gpinchon
 * @Date:   2021-09-26 00:00:00
 * @Last Modified by:   gpinchon
-* @Last Modified time: 2021-09-26 14:31:47
+* @Last Modified time: 2021-10-04 20:32:43
 */
 #include <GL/Image/Format.hpp>
 #include <Handle.hpp>
@@ -15,69 +15,69 @@
 
 namespace OCRA::Image {
 struct Impl {
-	Impl(const Device::Handle& a_Device, const Info& a_Info)
-	: info(a_Info)
-	{
-		glGenTextures(1, &handle);
-		auto internalformat { GetGLFormat(a_Info.format) };
-		if (a_Info.samples == Info::Samples::Samples1) {
-			switch (a_Info.type) {
-			case Image::Info::Type::Image1D:
-				glTextureStorage1D(
-					handle,
-					a_Info.mipLevels,
-					internalformat,
-					a_Info.extent.width);
-				break;
-			case Image::Info::Type::Image2D:
-				glTextureStorage2D(
-					handle,
-					a_Info.mipLevels,
-					internalformat,
-					a_Info.extent.width,
-					a_Info.extent.height);
-				break;
-			case Image::Info::Type::Image3D:
-				glTextureStorage3D(
-					handle,
-					a_Info.mipLevels,
-					internalformat,
-					a_Info.extent.width,
-					a_Info.extent.height,
-					a_Info.extent.depth);
-				break;
-			default:
-				throw std::runtime_error("Unknown Image Type");
-			}
-		} else {
-			switch (a_Info.type) {
-			case Image::Info::Type::Image1D:
-				throw std::runtime_error("Cannot create multisampled 1D textures");
-				break;
-			case Image::Info::Type::Image2D:
-				glTextureStorage2DMultisample(
-					handle,
-					Uint32(a_Info.samples),
-					internalformat,
-					a_Info.extent.width,
-					a_Info.extent.height,
-					a_Info.fixedSampleLocations);
-				break;
-			case Image::Info::Type::Image3D:
-				glTextureStorage3DMultisample(
-					handle,
-					Uint32(a_Info.samples),
-					a_Info.mipLevels,
-					internalformat,
-					a_Info.extent.width,
-					a_Info.extent.height,
-					a_Info.fixedSampleLocations);
-				break;
-			default:
-				throw std::runtime_error("Unknown multisampled Image Type");
-			}
-		}
-	}
+    Impl(const Device::Handle& a_Device, const Info& a_Info)
+        : info(a_Info)
+    {
+        glGenTextures(1, &handle);
+        auto internalformat { GetGLFormat(a_Info.format) };
+        if (a_Info.samples == Info::Samples::Samples1) {
+            switch (a_Info.type) {
+            case Image::Info::Type::Image1D:
+                glTextureStorage1D(
+                    handle,
+                    a_Info.mipLevels,
+                    internalformat,
+                    a_Info.extent.width);
+                break;
+            case Image::Info::Type::Image2D:
+                glTextureStorage2D(
+                    handle,
+                    a_Info.mipLevels,
+                    internalformat,
+                    a_Info.extent.width,
+                    a_Info.extent.height);
+                break;
+            case Image::Info::Type::Image3D:
+                glTextureStorage3D(
+                    handle,
+                    a_Info.mipLevels,
+                    internalformat,
+                    a_Info.extent.width,
+                    a_Info.extent.height,
+                    a_Info.extent.depth);
+                break;
+            default:
+                throw std::runtime_error("Unknown Image Type");
+            }
+        } else {
+            switch (a_Info.type) {
+            case Image::Info::Type::Image1D:
+                throw std::runtime_error("Cannot create multisampled 1D textures");
+                break;
+            case Image::Info::Type::Image2D:
+                glTextureStorage2DMultisample(
+                    handle,
+                    Uint32(a_Info.samples),
+                    internalformat,
+                    a_Info.extent.width,
+                    a_Info.extent.height,
+                    a_Info.fixedSampleLocations);
+                break;
+            case Image::Info::Type::Image3D:
+                glTextureStorage3DMultisample(
+                    handle,
+                    Uint32(a_Info.samples),
+                    a_Info.mipLevels,
+                    internalformat,
+                    a_Info.extent.width,
+                    a_Info.extent.height,
+                    a_Info.fixedSampleLocations);
+                break;
+            default:
+                throw std::runtime_error("Unknown multisampled Image Type");
+            }
+        }
+    }
     ~Impl()
     {
         glDeleteTextures(1, &handle);
@@ -103,6 +103,6 @@ const Info& GetInfo(const Device::Handle& a_Device, const Handle& a_Handle)
 }
 unsigned GetGLHandle(const Device::Handle& a_Device, const Handle& a_Handle)
 {
-	return s_Images.at(a_Handle).handle;
+    return s_Images.at(a_Handle).handle;
 }
 }
