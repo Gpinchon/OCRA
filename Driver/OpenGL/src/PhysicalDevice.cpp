6867fc7ddc5e2df13b9c24536facfa46f32e169b
gpinchon <pinchon.guillaume@gmail.com>
2022-03-14 23:45:59 +0100

    PhysicalDevice Properties WIP

--- Driver/OpenGL/src/PhysicalDevice.cpp
+++ Driver/OpenGL/src/PhysicalDevice.cpp
@@ -1,11 +1,209 @@
 #include <PhysicalDevice.hpp>
 
+#define _WIN32_DCOM
+#include <iostream>
+#include <comdef.h>
+#include <Wbemidl.h>
+#include <Rpcdce.h>
+#pragma comment(lib, "wbemuuid.lib")
+
+#include <d3d9.h>
+
 namespace OCRA::PhysicalDevice
 {
+//DirectX to the rescue !
+int GetPhysicalDeviceDX9()
+{
+    auto pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+    D3DADAPTER_IDENTIFIER9  d3dIdentifier;
+    D3DCAPS9                d3dCaps;
+    D3DDISPLAYMODE          d3dDisplayMode;
+    pD3D->GetAdapterIdentifier(
+        D3DADAPTER_DEFAULT,
+        0,
+        &d3dIdentifier);
+    pD3D->GetDeviceCaps(
+        D3DADAPTER_DEFAULT,
+        D3DDEVTYPE_HAL,
+        &d3dCaps);
+    pD3D->GetAdapterDisplayMode(
+        D3DADAPTER_DEFAULT,
+        &d3dDisplayMode);
+}
+int GetPhysicalDeviceWMI()
+{
+    HRESULT hres;
+
+    // Step 1: --------------------------------------------------
+    // Initialize COM. ------------------------------------------
+
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres))
+    {
+        std::cout << "Failed to initialize COM library. Error code = 0x"
+            << std::hex << hres << std::endl;
+        return 1;                  // Program has failed.
+    }
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,                          // COM authentication
+        NULL,                        // Authentication services
+        NULL,                        // Reserved
+        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+        NULL,                        // Authentication info
+        EOAC_NONE,                   // Additional capabilities 
+        NULL                         // Reserved
+    );
+
+
+    if (FAILED(hres))
+    {
+        std::cout << "Failed to initialize security. Error code = 0x"
+            << std::hex << hres << std::endl;
+        CoUninitialize();
+        return 1;                    // Program has failed.
+    }
+
+    // Step 3: ---------------------------------------------------
+    // Obtain the initial locator to WMI -------------------------
+
+    IWbemLocator* pLoc = NULL;
+
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator, (LPVOID*)&pLoc);
+
+    if (FAILED(hres))
+    {
+        std::cout << "Failed to create IWbemLocator object."
+            << " Err code = 0x"
+            << std::hex << hres << std::endl;
+        CoUninitialize();
+        return 1;                 // Program has failed.
+    }
+
+    // Step 4: -----------------------------------------------------
+    // Connect to WMI through the IWbemLocator::ConnectServer method
+
+    IWbemServices* pSvc = NULL;
+
+    // Connect to the root\cimv2 namespace with
+    // the current user and obtain pointer pSvc
+    // to make IWbemServices calls.
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+        NULL,                    // User name. NULL = current user
+        NULL,                    // User password. NULL = current
+        0,                       // Locale. NULL indicates current
+        NULL,                    // Security flags.
+        0,                       // Authority (for example, Kerberos)
+        0,                       // Context object 
+        &pSvc                    // pointer to IWbemServices proxy
+    );
+
+    if (FAILED(hres))
+    {
+        std::cout << "Could not connect. Error code = 0x"
+            << std::hex << hres << std::endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;                // Program has failed.
+    }
+
+    std::cout << "Connected to ROOT\\CIMV2 WMI namespace" << std::endl;
+
+
+    // Step 5: --------------------------------------------------
+    // Set security levels on the proxy -------------------------
+
+    hres = CoSetProxyBlanket(
+        pSvc,                        // Indicates the proxy to set
+        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+        NULL,                        // Server principal name 
+        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+        NULL,                        // client identity
+        EOAC_NONE                    // proxy capabilities 
+    );
+
+    if (FAILED(hres))
+    {
+        std::cout << "Could not set proxy blanket. Error code = 0x"
+            << std::hex << hres << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // Step 6: --------------------------------------------------
+    // Use the IWbemServices pointer to make requests of WMI ----
+
+    // For example, get the name of the operating system
+    IEnumWbemClassObject* pEnumerator = NULL;
+    hres = pSvc->ExecQuery(
+        bstr_t("WQL"),
+        bstr_t("SELECT * FROM Win32_VideoController"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        NULL,
+        &pEnumerator);
+
+    if (FAILED(hres))
+    {
+        std::cout << "Query for operating system name failed."
+            << " Error code = 0x"
+            << std::hex << hres << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // Step 7: -------------------------------------------------
+    // Get the data from the query in step 6 -------------------
+
+    IWbemClassObject* pclsObj = NULL;
+    ULONG uReturn = 0;
+
+    while (pEnumerator)
+    {
+        HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1,
+            &pclsObj, &uReturn);
+
+        if (0 == uReturn)
+        {
+            break;
+        }
+
+        VARIANT vtProp;
+
+        // Get the value of the Name property
+        hr = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
+        std::cout << " GPU Name : " << vtProp.bstrVal << std::endl;
+        VariantClear(&vtProp);
+
+        pclsObj->Release();
+    }
+
+    // Cleanup
+    // ========
+
+    pSvc->Release();
+    pLoc->Release();
+    pEnumerator->Release();
+    CoUninitialize();
+
+    return 0;   // Program successfully completed.
+}
 struct Impl
 {
 	Impl()
 	{
+
 	}
 	Properties properties;
 };
