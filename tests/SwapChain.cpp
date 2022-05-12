ca4e4237ceb07f34fda3def51cd98a9747007941
gpinchon <pinchon.guillaume@gmail.com>
2022-05-12 23:08:56 +0200

    use TestApp in SwapChain

--- tests/SwapChain.cpp
+++ tests/SwapChain.cpp
@@ -65,63 +65,91 @@ Vec3 HSVtoRGB(float fH, float fS, float fV) {
     return { fR, fG, fB };
 }
 
-static inline auto RecordClearCommandBuffer(Command::Buffer::Handle a_CommandBuffer, const Image::Handle& a_Image)
+struct SwapChainTestApp : TestApp
 {
-    static float hue = 0;
-    static auto lastTime = std::chrono::high_resolution_clock::now();
-    const auto now = std::chrono::high_resolution_clock::now();
-    const auto delta = std::chrono::duration<double, std::milli>(now - lastTime).count();
-    lastTime = now;
-    hue += 0.05 * delta;
-    hue = hue > 360 ? 0 : hue;
-    const auto color = HSVtoRGB(hue, 0.5f, 1.f);
-    Command::Buffer::BeginInfo bufferBeginInfo;
-    bufferBeginInfo.flags = Command::Buffer::UsageFlagBits::None;
-    Command::Buffer::Reset(a_CommandBuffer);
-    Command::Buffer::Begin(a_CommandBuffer, bufferBeginInfo);
+    SwapChainTestApp
+        : TestApp("Test_SwapChain")
+        , window(Window("Test_SwapChain", 1280, 720))
+        , surface(CreateSurface(instance, GetModuleHandle(0), (void*)window.nativeHandle))
+        , physicalDevice(Instance::EnumeratePhysicalDevices(instance).front())
+        , device(CreateDevice(physicalDevice))
     {
-        ColorValue clearColor{ color.r, color.g, color.b, 1.f };
-        Image::Subresource::Range range{};
-        range.levelCount = 1;
-        Command::ClearColorImage(a_CommandBuffer, a_Image, Image::Layout::Unknown, clearColor, { range });
+        window.OnResize = [this](const Window&, const uint32_t a_Width, const uint32_t a_Height) {
+            OnResize(a_Width, a_Height);
+        };
+        window.OnClose = [this](const Window&) {
+            OnClose();
+        };
+        const auto queueFamily = FindQueueFamily(physicalDevice, PhysicalDevice::QueueFlagsBits::Graphics);
+        queue = Device::GetQueue(device, queueFamily, 0); //Get first available queue
+        imageAcquisitionFence = Queue::Fence::Create(device);
+        commandPool = CreateCommandPool(device, queueFamily);
+        commandBuffer = CreateCommandBuffer(device, commandPool, Command::Pool::AllocateInfo::Level::Primary);
     }
-    Command::Buffer::End(a_CommandBuffer);
+    void Loop()
+    {
+        while (true) {
+            window.PushEvents();
+            if (close) break;
+            const auto swapChainImage = SwapChain::AcquireNextImage(device, swapChain, std::chrono::nanoseconds(15000000), nullptr, imageAcquisitionFence);
+            Queue::Fence::WaitFor(device, imageAcquisitionFence, std::chrono::nanoseconds(15000000));
+            Queue::Fence::Reset(device, { imageAcquisitionFence });
+            RecordClearCommandBuffer(swapChainImage);
+            SubmitCommandBuffer(queue, commandBuffer);
+            SwapChain::Present(queue, presentInfo);
+            Queue::WaitIdle(queue);
+        }
+    }
+    void RecordClearCommandBuffer(const Image::Handle& a_Image)
+    {
+        static float hue = 0;
+        static auto lastTime = std::chrono::high_resolution_clock::now();
+        const auto now = std::chrono::high_resolution_clock::now();
+        const auto delta = std::chrono::duration<double, std::milli>(now - lastTime).count();
+        lastTime = now;
+        hue += 0.05 * delta;
+        hue = hue > 360 ? 0 : hue;
+        const auto color = HSVtoRGB(hue, 0.5f, 1.f);
+        Command::Buffer::BeginInfo bufferBeginInfo;
+        bufferBeginInfo.flags = Command::Buffer::UsageFlagBits::None;
+        Command::Buffer::Reset(commandBuffer);
+        Command::Buffer::Begin(commandBuffer, bufferBeginInfo);
+        {
+            ColorValue clearColor{ color.r, color.g, color.b, 1.f };
+            Image::Subresource::Range range{};
+            range.levelCount = 1;
+            Command::ClearColorImage(commandBuffer, a_Image, Image::Layout::Unknown, clearColor, { range });
+        }
+        Command::Buffer::End(commandBuffer);
+    }
+    void OnResize(const uint32_t a_Width, const uint32_t a_Height)
+    {
+        swapChain = CreateSwapChain(device, surface, swapChain, a_Width, a_Height, 2);
+        presentInfo.swapChains = { swapChain };
+        extent = { a_Width, a_Height };
+    }
+    void OnClose()
+    {
+        close = true;
+    }
+    bool                    close{ false };
+    Window                  window;
+    uExtent2D               extent;
+    Surface::Handle         surface;
+    PhysicalDevice::Handle  physicalDevice;
+    Device::Handle          device;
+    SwapChain::Handle       swapChain;
+    Queue::Handle           queue;
+    Queue::Fence::Handle    imageAcquisitionFence;
+    Command::Pool::Handle   commandPool;
+    Command::Buffer::Handle commandBuffer;
 }
 
 int SwapChain()
 {
     int ret = 0;
-    auto window = Window("Test_SwapChain", 1280, 720);
-    //basic setup as usual
-    const auto instance = CreateInstance("Test_SwapChain");
-    const auto surface = CreateSurface(instance, GetModuleHandle(0), (void*)window.nativeHandle);
-    const auto physicalDevice = Instance::EnumeratePhysicalDevices(instance).front();
-    const auto device = CreateDevice(physicalDevice);
-    const auto queueFamily = FindQueueFamily(physicalDevice, PhysicalDevice::QueueFlagsBits::Transfer);
-    const auto queue = Device::GetQueue(device, queueFamily, 0); //Get first available queue
-    const auto commandPool = CreateCommandPool(device, queueFamily);
-
-    Command::Buffer::Handle clearCommandBuffer = CreateCommandBuffer(device, commandPool, Command::Pool::AllocateInfo::Level::Primary);
-    SwapChain::Handle       swapChain;
-    SwapChain::PresentInfo  presentInfo;
-    bool close = false;
-
-    window.OnResize = [&device, &surface, &swapChain, &presentInfo](const Window&, const uint32_t a_Width, const uint32_t a_Height) {
-        swapChain = CreateSwapChain(device, surface, swapChain, a_Width, a_Height, SWAPCHAIN_IMAGE_NBR);
-        presentInfo.swapChains = { swapChain };
-    };
-    window.OnClose = [&close](const Window&) {
-        close = true;
-    };
-    window.Show();
-    while (true) {
-        window.PushEvents();
-        if (close) break;
-        const auto swapChainImage = SwapChain::AcquireNextImage(device, swapChain, std::chrono::nanoseconds(15000000), nullptr, nullptr);
-        RecordClearCommandBuffer(clearCommandBuffer, swapChainImage);
-        SubmitCommandBuffer(queue, clearCommandBuffer);
-        SwapChain::Present(queue, presentInfo);
-        Queue::WaitIdle(queue);
-    }
+    SwapChainTestApp testApp;
+    testApp.window.Show();
+    testApp.Loop();
 	return ret;
 }
