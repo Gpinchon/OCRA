6d0def2c6dc29cea7e2024488709a62b12fdf39e
gpinchon <pinchon.guillaume@gmail.com>
2022-06-12 23:32:17 +0200

    GraphicsPipeline Unit-Test Mat4x4 WIP

--- UnitTests/GraphicsPipeline/src/GraphicsPipeline.cpp
+++ UnitTests/GraphicsPipeline/src/GraphicsPipeline.cpp
@@ -28,6 +28,77 @@ using namespace OCRA;
 
 #include <Pipeline/Graphics.hpp>
 
+namespace OCRA {
+template <size_t C, size_t R, typename T>
+struct Mat;
+template <typename T>
+struct Mat<4, 4, T> {
+    typedef T value_type;
+    typedef Mat<4, 4, T> type;
+    typedef Vec<4, T> col_type;
+    typedef Vec<4, T> row_type;
+
+    Mat() : value{
+        col_type(1, 0, 0, 0),
+        col_type(0, 1, 0, 0),
+        col_type(0, 0, 1, 0),
+        col_type(0, 0, 0, 1) }
+    {}
+
+    template<typename V1, typename V2, typename V3, typename V4>
+    Mat(col_type const& a_V1,
+        col_type const& a_V2,
+        col_type const& a_V3,
+        col_type const& a_V4)
+    {
+        value[0] = a_V1;
+        value[1] = a_V2;
+        value[2] = a_V3;
+        value[3] = a_V4;
+    }
+    auto& operator[](size_t const& a_Col) {
+        return value[a_Col];
+    }
+    auto& operator[](size_t const& a_Col) const {
+        return value[a_Col];
+    }
+    template<typename T>
+    auto Rotate(Mat<4, 4, T> const& a_M, T a_Angle, Vec<3, T> const& a_V)
+    {
+        T const a = a_Angle;
+        T const c = cos(a);
+        T const s = sin(a);
+
+        auto axis(Normalize(a_V));
+        auto temp((T(1) - c) * axis);
+
+        Mat<4, 4, T> Rotate;
+        Rotate[0][0] = c + temp[0] * axis[0];
+        Rotate[0][1] = temp[0] * axis[1] + s * axis[2];
+        Rotate[0][2] = temp[0] * axis[2] - s * axis[1];
+
+        Rotate[1][0] = temp[1] * axis[0] - s * axis[2];
+        Rotate[1][1] = c + temp[1] * axis[1];
+        Rotate[1][2] = temp[1] * axis[2] + s * axis[0];
+
+        Rotate[2][0] = temp[2] * axis[0] + s * axis[1];
+        Rotate[2][1] = temp[2] * axis[1] - s * axis[0];
+        Rotate[2][2] = c + temp[2] * axis[2];
+
+        Mat<4, 4, T> Result;
+        Result[0] = a_M[0] * Rotate[0][0] + a_M[1] * Rotate[0][1] + a_M[2] * Rotate[0][2];
+        Result[1] = a_M[0] * Rotate[1][0] + a_M[1] * Rotate[1][1] + a_M[2] * Rotate[1][2];
+        Result[2] = a_M[0] * Rotate[2][0] + a_M[1] * Rotate[2][1] + a_M[2] * Rotate[2][2];
+        Result[3] = a_M[3];
+        return Result;
+    }
+
+private:
+    col_type value[4];
+};
+typedef Mat<4, 4, float> Mat4x4;
+}
+
 Vec3 HSVtoRGB(float fH, float fS, float fV) {
     float fC = fV * fS; // Chroma
     float fHPrime = fmod(fH / 60.0, 6);
