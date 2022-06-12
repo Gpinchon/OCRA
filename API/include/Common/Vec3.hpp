80672fe3a699dbb8b17ceec1005634a52dcd385f
gpinchon <pinchon.guillaume@gmail.com>
2022-06-12 18:54:42 +0200

    add basic arithmetic for Vec3 & Vec4

--- API/include/Common/Vec3.hpp
+++ API/include/Common/Vec3.hpp
@@ -8,11 +8,75 @@ struct Vec<3, T> {
 	typedef T value_type;
 	typedef Vec<3, T> type;
 	constexpr auto length() const { return 3; }
+	Vec() : value{ 0, 0, 0 } {}
+	template<typename X, typename Y, typename Z>
+	Vec(const X& a_X = X(0), const Y& a_Y = Y(0), const Z& a_Z = Z(0))
+		: x(a_X), y(a_Y), z(a_Z)
+	{}
+	auto& operator[] (const size_t& a_Index) { return value[a_Index]; }
+	auto& operator[] (const size_t& a_Index) const { return value[a_Index]; }
 	union {
-		struct { T x, y, z; };
-		struct { T r, g, b; };
-		struct { T u, v, s; };
+		struct { value_type x, y, z; };
+		struct { value_type r, g, b; };
+		struct { value_type u, v, s; };
+		value_type value[3];
 	};
 };
-typedef Vec<3, float> Vec3;
+
+template<typename T>
+auto operator*(const T& a_Scalar, Vec<3, T> const& a_V)
+{
+	return Vec<3, T>(
+		a_Scalar * a_V.x,
+		a_Scalar * a_V.y,
+		a_Scalar * a_V.z);
+}
+
+template<typename T>
+auto operator*(Vec<3, T> const& a_V, const T& a_Scalar)
+{
+	return Vec<3, T>(
+		a_V.x * a_Scalar,
+		a_V.y * a_Scalar,
+		a_V.z * a_Scalar);
+}
+
+template<typename T>
+auto operator/(const T& a_Scalar, Vec<3, T> const& a_V)
+{
+	return Vec<3, T>(
+		a_Scalar / a_V.x,
+		a_Scalar / a_V.y,
+		a_Scalar / a_V.z);
+}
+
+template<typename T>
+auto operator/(Vec<3, T> const& a_V, const T& a_Scalar)
+{
+	return Vec<3, T>(
+		a_V.x / a_Scalar,
+		a_V.y / a_Scalar,
+		a_V.z / a_Scalar);
+}
+
+template<typename T>
+auto Normalize(Vec<3, T> const& a_V)
+{
+	auto v = a_V;
+	auto len = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+
+	if (len != T(0))
+	{
+		v.x /= len;
+		v.y /= len;
+		v.z /= len;
+	}
+	return v;
+}
+
+typedef Vec<3, float>	 Vec3;
+typedef Vec<3, double>	 dVec3;
+typedef Vec<3, bool>	 bVec3;
+typedef Vec<3, int>		 iVec3;
+typedef Vec<3, unsigned> uVec3;
 }
--- API/include/Common/Vec4.hpp
+++ API/include/Common/Vec4.hpp
@@ -8,11 +8,120 @@ struct Vec<4, T> {
 	typedef T value_type;
 	typedef Vec<4, T> type;
 	constexpr auto length() const { return 4; }
+	Vec() : value{ 0, 0, 0, 0 } {}
+	template<typename X, typename Y, typename Z, typename W>
+	Vec(const X& a_X = X(0), const Y& a_Y = Y(0), const Z& a_Z = Z(0), const W& a_W = W(0))
+		: x(a_X), y(a_Y), z(a_Z), w(a_W)
+	{}
+	auto& operator[] (const size_t a_Index) { return value[a_Index]; }
+	auto& operator[] (const size_t a_Index) const { return value[a_Index]; }
 	union {
-		struct { T x, y, z, w; };
-		struct { T r, g, b, a; };
-		struct { T u, v, s, t; };
+		struct { value_type x, y, z, w; };
+		struct { value_type r, g, b, a; };
+		struct { value_type u, v, s, t; };
+		value_type value[4];
 	};
 };
-typedef Vec<4, float> Vec4;
+
+template<typename T>
+auto operator*(const T& a_Scalar, Vec<4, T> const& a_V)
+{
+	return Vec<4, T>(
+		a_Scalar * a_V.x,
+		a_Scalar * a_V.y,
+		a_Scalar * a_V.z,
+		a_Scalar * a_V.w);
+}
+
+template<typename T>
+auto operator*(Vec<4, T> const& a_V, const T& a_Scalar)
+{
+	return Vec<4, T>(
+		a_V.x * a_Scalar,
+		a_V.y * a_Scalar,
+		a_V.z * a_Scalar,
+		a_V.w * a_Scalar);
+}
+
+template<typename T>
+auto operator/(const T& a_Scalar, Vec<4, T> const& a_V)
+{
+	return Vec<4, T>(
+		a_Scalar / a_V.x,
+		a_Scalar / a_V.y,
+		a_Scalar / a_V.z,
+		a_Scalar / a_V.w);
+}
+
+template<typename T>
+auto operator/(Vec<4, T> const& a_V, const T& a_Scalar)
+{
+	return Vec<4, T>(
+		a_V.x / a_Scalar,
+		a_V.y / a_Scalar,
+		a_V.z / a_Scalar,
+		a_V.w / a_Scalar);
+}
+
+template<typename V1, typename V2>
+auto operator+(Vec<4, V1> const& a_V1, Vec<4, V2> const& a_V2)
+{
+	return Vec<4, V1>(
+		a_V1.x + a_V2.x,
+		a_V1.y + a_V2.y,
+		a_V1.z + a_V2.z,
+		a_V1.w + a_V2.w);
+}
+
+template<typename V1, typename V2>
+auto operator-(Vec<4, V1> const& a_V1, Vec<4, V2> const& a_V2)
+{
+	return Vec<4, V1>(
+		a_V1.x - a_V2.x,
+		a_V1.y - a_V2.y,
+		a_V1.z - a_V2.z,
+		a_V1.w - a_V2.w);
+}
+
+template<typename V1, typename V2>
+auto operator*(Vec<4, V1> const& a_V1, Vec<4, V2> const& a_V2)
+{
+	return Vec<4, V1>(
+		a_V1.x * a_V2.x,
+		a_V1.y * a_V2.y,
+		a_V1.z * a_V2.z,
+		a_V1.w * a_V2.w);
+}
+
+template<typename V1, typename V2>
+auto operator/(Vec<4, V1> const& a_V1, Vec<4, V2> const& a_V2)
+{
+	return Vec<4, V1>(
+		a_V1.x / a_V2.x,
+		a_V1.y / a_V2.y,
+		a_V1.z / a_V2.z,
+		a_V1.w / a_V2.w);
+}
+
+template<typename T>
+auto Normalize(Vec<4, T> const& a_V)
+{
+	auto v = a_V;
+	auto len = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w);
+
+	if (len != T(0))
+	{
+		v.x /= len;
+		v.y /= len;
+		v.z /= len;
+		v.w /= len;
+	}
+	return v;
+}
+
+typedef Vec<4, float>	 Vec4;
+typedef Vec<4, double>	 dVec4;
+typedef Vec<4, bool>	 bVec4;
+typedef Vec<4, int>		 iVec4;
+typedef Vec<4, unsigned> uVec4;
 }
