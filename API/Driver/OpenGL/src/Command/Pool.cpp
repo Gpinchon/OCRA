b44540eb4d9bfb8cd8463cda254b7274d17c9618
gpinchon <pinchon.guillaume@gmail.com>
2022-10-26 16:26:56 +0200

    add debugging mechanism for Pools

--- API/Driver/OpenGL/src/Command/Pool.cpp
+++ API/Driver/OpenGL/src/Command/Pool.cpp
@@ -11,19 +11,31 @@ struct AllocationCallback;
 }
 
 OCRA_DECLARE_HANDLE(OCRA::Command::Buffer);
-OCRA_DECLARE_WEAK_HANDLE(OCRA::Command::Pool);
+#ifdef _DEBUG
+OCRA_DECLARE_WEAK_HANDLE(OCRA::Command::Buffer);
+#endif
 
 namespace OCRA::Command::Pool
 {
+constexpr auto MaxCommandBufferNbr = 4096;
 struct Impl
 {
     Impl(const Device::Handle& a_Device, const Info& a_Info)
         : info(a_Info)
     {}
-    std::pmr::unsynchronized_pool_resource memoryPool;
-    std::pmr::polymorphic_allocator<Buffer::Impl>    bufferAllocator{ &memoryPool };
-    //std::pmr::polymorphic_allocator<Buffer::Command> commandAllocator{ &memoryPool };
+#ifdef _DEBUG
+    ~Impl() {
+        //If this assert fails, this pool was destroyed before the objects it allocated
+        for (auto& allocated : allocated) assert(allocated.expired());
+    }
+#endif
     const Info info;
+    std::byte                                       memory[sizeof(Buffer::Impl) * MaxCommandBufferNbr];
+    std::pmr::monotonic_buffer_resource             memoryPool{ memory, sizeof(Buffer::Impl) * MaxCommandBufferNbr };
+    std::pmr::polymorphic_allocator<Buffer::Impl>   bufferAllocator{ &memoryPool };
+#ifdef _DEBUG
+    std::vector<Buffer::WeakHandle>                 allocated;
+#endif
 };
 Handle Create(
     const Device::Handle&       a_Device,
@@ -43,6 +55,12 @@ std::vector<Buffer::Handle> AllocateBuffer(const Device::Handle& a_Device, const
     for (auto i = 0u; i < a_Info.count; ++i) {
         auto buffer = std::allocate_shared<Buffer::Impl>(a_Info.pool->bufferAllocator, a_Device, Buffer::Level(a_Info.level));
         commandBuffers.push_back(buffer);
+#ifdef _DEBUG
+        auto& allocated = a_Info.pool->allocated;
+        allocated.push_back(buffer);
+        //cleanup while we're at it
+        allocated.erase(std::remove_if(allocated.begin(), allocated.end(), [](auto& allocated) { return allocated.expired(); }), allocated.end());
+#endif
     }
     return commandBuffers;
 }
--- API/Driver/OpenGL/src/Descriptor/Pool.cpp
+++ API/Driver/OpenGL/src/Descriptor/Pool.cpp
@@ -6,7 +6,9 @@
 #include <vector>
 
 OCRA_DECLARE_HANDLE(OCRA::Descriptor::Set);
-OCRA_DECLARE_WEAK_HANDLE(OCRA::Descriptor::Pool);
+#ifdef _DEBUG
+OCRA_DECLARE_WEAK_HANDLE(OCRA::Descriptor::Set);
+#endif
 
 namespace OCRA {
 struct AllocationCallback;
@@ -19,9 +21,19 @@ struct Impl
 	Impl(const Device::Handle& a_Device, const Info& a_Info)
 		: info(a_Info)
 	{}
-	std::pmr::unsynchronized_pool_resource		memoryPool;
+#ifdef _DEBUG
+	~Impl() {
+		//If this assert fails, this pool was destroyed before the objects it allocated
+		for (auto& allocated : allocated) assert(allocated.expired());
+	}
+#endif
+	const Info									info;
+	std::vector<std::byte>						memory{ sizeof(Set::Impl) * info.maxSets };
+	std::pmr::monotonic_buffer_resource			memoryPool{ memory.data(), memory.size() };
 	std::pmr::polymorphic_allocator<Set::Impl>	allocator{ &memoryPool };
-	const Info info;
+#ifdef _DEBUG
+	std::vector<Set::WeakHandle>				allocated;
+#endif
 };
 Handle Create(
 	const Device::Handle&		a_Device,
@@ -41,6 +53,12 @@ std::vector<Set::Handle> AllocateSet(const Device::Handle& a_Device, const Alloc
 	for (const auto& layout : a_Info.layouts) {
 		auto set = std::allocate_shared<Set::Impl>(a_Info.pool->allocator, layout);
 		sets.push_back(set);
+#ifdef _DEBUG
+		auto& allocated = a_Info.pool->allocated;
+		allocated.push_back(set);
+		//cleanup while we're at it
+		allocated.erase(std::remove_if(allocated.begin(), allocated.end(), [](auto& allocated) { return allocated.expired(); }), allocated.end());
+#endif
 	}
 	return sets;
 }
