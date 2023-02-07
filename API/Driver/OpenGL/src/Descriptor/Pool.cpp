bbf3ec7abad59e3f1401ae226fe6e6d6751bbfe4
gpinchon <pinchon.guillaume@gmail.com>
2023-02-07 18:13:15 +0100

    simplify & optimize Descriptor Pool

--- API/Driver/OpenGL/src/Descriptor/Pool.cpp
+++ API/Driver/OpenGL/src/Descriptor/Pool.cpp
@@ -1,13 +1,14 @@
 #include <OCRA/Descriptor/Pool.hpp>
 
-#include <GL/Common/MemoryPool.hpp>
 #include <GL/Descriptor/Set.hpp>
 #include <GL/Descriptor/SetLayout.hpp>
 #include <GL/Descriptor/Binding.hpp>
 
+#include <memory_resource>
+
+#ifdef _DEBUG
 #include <vector>
-#include <array>
-#include <functional>
+#endif
 
 OCRA_DECLARE_HANDLE(OCRA::Descriptor::Set);
 #ifdef _DEBUG
@@ -28,40 +29,17 @@ auto GetTypeCount(const Info& a_Info, const Type& a_Type) {
     return count;
 }
 
-namespace detail
-{
-
-    template <typename T, std::size_t...Is>
-    constexpr std::array<T, sizeof...(Is)>
-        make_array(std::index_sequence<Is...>, const std::function<T(size_t)>& a_Func)
-    {
-        return { a_Func(Is)... };
-    }
-}
-
-template <typename T, std::size_t N>
-constexpr std::array<T, N> make_array(const std::function<T(size_t)>& a_Func)
-{
-    return detail::make_array(std::make_index_sequence<N>(), a_Func);
-}
-
 struct Impl
 {
     Impl(const Device::Handle& a_Device, const Info& a_Info)
-        : sets(a_Info.maxSets)
-        , bindings(make_array<MemoryPool<Binding>, size_t(Type::MaxValue)>([a_Info](size_t a_TypeIndex) {
-            return GetTypeCount(a_Info, Type(a_TypeIndex));
-        }))
-    {
-    }
+    {}
 #ifdef _DEBUG
     ~Impl() {
         //If this assert fails, this pool was destroyed before the objects it allocated
         for (auto& allocated : allocated) assert(allocated.expired());
     }
 #endif
-    MemoryPool<Set::Impl>                       sets;
-    std::array<MemoryPool<Binding>, size_t(Type::MaxValue)> bindings;
+    std::pmr::unsynchronized_pool_resource memory_resource;
 #ifdef _DEBUG
     std::vector<Set::WeakHandle>  allocated;
 #endif
@@ -77,13 +55,11 @@ Handle Create(
 
 Set::Handle AllocateSet(const Device::Handle& a_Device, const AllocateInfo& a_Info)
 {
-    auto& setsPool = a_Info.pool->sets;
-    auto set = std::shared_ptr<Set::Impl>(new(setsPool.allocate()) Set::Impl(), setsPool.deleter());
+    auto& memoryResource = a_Info.pool->memory_resource;
+    auto set = std::allocate_shared<Set::Impl>(std::pmr::polymorphic_allocator<Set::Impl>(&memoryResource));
     for (auto& binding : a_Info.layout->bindings) {
-        auto& bindingsPool = a_Info.pool->bindings.at(size_t(binding.type));
         for (size_t bindingIndex = 0; bindingIndex < binding.count; ++bindingIndex) {
-            auto storage = bindingsPool.allocate();
-            auto setBinding = std::shared_ptr<Binding>(new(storage) Binding(binding.type, binding.binding + bindingIndex), bindingsPool.deleter());
+            auto setBinding = std::allocate_shared<Binding>(std::pmr::polymorphic_allocator<Binding>(&memoryResource), binding.type, binding.binding + bindingIndex);
             set->bindings.at(bindingIndex) = setBinding;
             set->bindingCount++;
         }
