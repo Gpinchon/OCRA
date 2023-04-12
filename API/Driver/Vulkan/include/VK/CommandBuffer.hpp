b746ac0bb5633b3e360741608a87f105c8133a84
gpinchon <pinchon.guillaume@gmail.com>
2023-04-13 00:18:05 +0200

    add image layout tracking for CommandBuffers

--- API/Driver/Vulkan/include/VK/CommandBuffer.hpp
+++ API/Driver/Vulkan/include/VK/CommandBuffer.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <OCRA/Enums.hpp>
+
 #include <vulkan/vulkan_raii.hpp>
+
+#include <unordered_map>
 
 namespace OCRA::Command::Buffer
 {
 struct Impl : vk::raii::CommandBuffer
 {
-    Impl(vk::raii::CommandBuffer&& a_CommandBuffer)
+    Impl(vk::raii::CommandBuffer&& a_CommandBuffer, const vk::CommandBufferLevel& a_Level)
         : vk::raii::CommandBuffer(std::move(a_CommandBuffer))
+        , level(a_Level)
     {}
+    const vk::CommandBufferLevel level;
+    std::unordered_map<VkImage, ImageLayout> imageLayouts;
 };
 }
