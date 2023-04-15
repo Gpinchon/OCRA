f7009542fa5d4de4bccd77035c2d20bdd71fe34f
gpinchon <pinchon.guillaume@gmail.com>
2023-04-16 01:26:47 +0200

    use constexpr for Extent constructors

--- API/include/OCRA/Extent.hpp
+++ API/include/OCRA/Extent.hpp
@@ -9,7 +9,7 @@ struct Extent<2, T> {
     typedef T value_type;
     typedef Extent<2, T> type;
     constexpr auto length() { return 2; }
-    Extent<2, T>(const T& a_Width = T(0), const T& a_Height = T(0)) : width(a_Width), height(a_Height) {}
+    constexpr Extent<2, T>(const T& a_Width = T(0), const T& a_Height = T(0)) : width(a_Width), height(a_Height) {}
     struct { T width, height; };
 };
 template <typename T>
@@ -17,13 +17,13 @@ struct Extent<3, T> {
     typedef T value_type;
     typedef Extent<3, T> type;
     constexpr auto length() { return 3; }
-    Extent(const T& a_Width = T(0), const T& a_Height = T(0), const T& a_Depth = T(0)) : width(a_Width), height(a_Height), depth(a_Depth) {}
+    constexpr Extent(const T& a_Width = T(0), const T& a_Height = T(0), const T& a_Depth = T(0)) : width(a_Width), height(a_Height), depth(a_Depth) {}
     template<typename W, typename H, typename D>
-    Extent(const W& a_Width, const H& a_Height, const D& a_Depth) : Extent(T(a_Width), T(a_Height), T(a_Depth)) {}
+    constexpr Extent(const W& a_Width, const H& a_Height, const D& a_Depth) : Extent(T(a_Width), T(a_Height), T(a_Depth)) {}
     template<typename W, typename H>
-    Extent(const W& a_Width, const H& a_Height) : Extent(T(a_Width), T(a_Height)) {}
+    constexpr Extent(const W& a_Width, const H& a_Height) : Extent(T(a_Width), T(a_Height)) {}
     template<typename W>
-    Extent(const W& a_Width) : Extent(T(a_Width)) {}
+    constexpr Extent(const W& a_Width) : Extent(T(a_Width)) {}
     struct { T width, height, depth; };
 };
 
