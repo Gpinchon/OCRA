c869de14502309265e96e91470ad1e90a4c9e09c
gpinchon <pinchon.guillaume@gmail.com>
2023-06-17 12:21:07 +0200

    cleanup command buffer impl

--- API/Driver/Vulkan/include/VK/CommandBuffer.hpp
+++ API/Driver/Vulkan/include/VK/CommandBuffer.hpp
@@ -8,17 +8,11 @@
 
 namespace OCRA::Command::Buffer {
 struct Impl : vk::raii::CommandBuffer {
-    Impl(vk::raii::CommandBuffer&& a_CommandBuffer, const vk::CommandBufferLevel& a_Level)
+    Impl(vk::raii::CommandBuffer&& a_CommandBuffer,
+        const vk::CommandBufferLevel& a_Level)
         : vk::raii::CommandBuffer(std::move(a_CommandBuffer))
         , level(a_Level)
-    {
-    }
-    /*auto GetImageLayout(const vk::Image& a_Image) const {
-        auto res = imageLayouts.find(a_Image);
-        if (res != imageLayouts.end()) return res->second;
-        return ImageLayout::Undefined;
-    }*/
+    {}
     const vk::CommandBufferLevel level;
-    // std::unordered_map<VkImage, ImageLayout> imageLayouts;
 };
 }
