606ac6e7c601712616dbf9e41cedbe8b8407a133
gpinchon <pinchon.guillaume@gmail.com>
2023-07-10 23:41:18 +0200

    update ShaderCompiler test

--- UnitTests/Functionalities/src/ShaderCompiler.cpp
+++ UnitTests/Functionalities/src/ShaderCompiler.cpp
@@ -6,6 +6,83 @@
 
 using namespace OCRA;
 
+void VerifyArray(ShaderCompiler::CompiledShader& a_CompiledShader)
+{
+    ASSERT_FALSE(a_CompiledShader.SPIRVBinary.empty());
+    ASSERT_EQ(a_CompiledShader.uniformBuffers.size(), 2);
+    auto& materials = a_CompiledShader.GetUniformBuffers().at(1);
+    ASSERT_EQ(materials.name, "uMaterials");
+    ASSERT_EQ(materials.type.baseType, ShaderCompiler::ShaderResourceBaseType::Struct);
+    auto& materialsStruct = *materials.type.structure;
+    ASSERT_EQ(materialsStruct.members.size(), 2);
+    auto& intensity = materialsStruct[0];
+    auto& data = materialsStruct[1];
+    ASSERT_EQ(intensity.name, "intensity");
+    ASSERT_EQ(intensity.type.baseType, ShaderCompiler::ShaderResourceBaseType::Float);
+    ASSERT_EQ(data.name, "data");
+    ASSERT_EQ(data.type.baseType, ShaderCompiler::ShaderResourceBaseType::Struct);
+    ASSERT_EQ(data.type.arraySize.size(), 1);
+    ASSERT_EQ(data.type.arraySize.at(0), 4);
+}
+
+TEST(ShaderCompiler, Array)
+{
+    auto compiler = ShaderCompiler::Create();
+    ShaderCompiler::ShaderInfo shaderInfo;
+    shaderInfo.type = ShaderCompiler::ShaderType::Vertex;
+    shaderInfo.entryPoint = "array";
+    shaderInfo.source = {
+        "#version 430                                                     \n"
+        "struct LightData {                                               \n"
+        "   vec3 position;                                                \n"
+        "   vec3 color;                                                   \n"
+        "};                                                               \n"
+        "struct SpecGlossMaterial {                                       \n"
+        "   vec3 albedo;                                                  \n"
+        "   float spec;                                                   \n"
+        "   float gloss;                                                  \n"
+        "};                                                               \n"
+        "struct MetRoughMaterial {                                        \n"
+        "   vec3  albedo;                                                 \n"
+        "   float metallic;                                               \n"
+        "   float roughness;                                              \n"
+        "};                                                               \n"
+        "struct MaterialData {                                            \n"
+        "   float alpha;                                                  \n"
+        "   SpecGlossMaterial specGloss;                                  \n"
+        "   MetRoughMaterial  metRough;                                   \n"
+        "};                                                               \n"
+        "layout(location = 0) in  vec3 vPosition;                         \n"
+        "layout(location = 0) out vec3 vLight;                            \n"
+        "layout(location = 100, binding = 32) uniform sampler2D uScreen[10];\n"
+        "layout(binding = 0) uniform camera {                             \n"
+        "   mat4 viewMatrix;                                              \n"
+        "} uCameras[4];                                                   \n"
+        "layout(binding = 1) uniform material {                           \n"
+        "   vec4 intensity;                                               \n"
+        "   MaterialData data[4];                                         \n"
+        "} uMaterials;                                                    \n"
+        "layout(std430, binding = 0) buffer light {                       \n"
+        "   LightData data[];                                             \n"
+        "} sLights;                                                       \n"
+        "void array() {                                                   \n"
+        "   gl_Position = uCameras[0].viewMatrix * vec4(vPosition, 1);    \n"
+        "   vLight = vec3(0);                                             \n"
+        "   for(int i = 0; i < sLights.data.length(); ++i){               \n"
+        "      float dist = distance(vPosition, sLights.data[i].position);\n"
+        "      vLight += sLights.data[i].color * dist;                    \n"
+        "   }                                                             \n"
+        "}                                                                \n"
+        "void main() {                                                    \n"
+        "   array();                                                      \n"
+        "}                                                                \n"
+    };
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::OpenGL;
+    VerifyArray(ShaderCompiler::Compile(compiler, shaderInfo));
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::Vulkan;
+    VerifyArray(ShaderCompiler::Compile(compiler, shaderInfo));    
+}
+
 TEST(ShaderCompiler, EmptyShader)
 {
     auto compiler = ShaderCompiler::Create();
@@ -55,6 +132,17 @@ TEST(ShaderCompiler, VertexIndex)
     }
 }
 
+void VerifyPushConstant(ShaderCompiler::CompiledShader& a_CompiledShader)
+{
+    ASSERT_FALSE(a_CompiledShader.SPIRVBinary.empty());
+    ASSERT_EQ(a_CompiledShader.pushConstants.size(), 1);
+    ASSERT_EQ(a_CompiledShader.GetPushConstants().at(0).name, "PushConstants");
+    ASSERT_EQ(a_CompiledShader.GetPushConstants().at(0).type.baseType, OCRA::ShaderCompiler::ShaderResourceBaseType::Struct);
+    ASSERT_EQ(a_CompiledShader.GetPushConstants().at(0).type.structure->members.at(0).name, "viewMatrix");
+    ASSERT_EQ(a_CompiledShader.stageInputs.size(), 1);
+    ASSERT_EQ(a_CompiledShader.GetStageInputs().at(0).name, "vPosition");
+}
+
 TEST(ShaderCompiler, PushConstant)
 {
     int ret       = 0;
@@ -75,28 +163,10 @@ TEST(ShaderCompiler, PushConstant)
             "   pushConstant();                                             \n"
             "}                                                              \n"
     };
-    {
-        shaderInfo.targetAPI = ShaderCompiler::TargetAPI::OpenGL;
-        auto compiledShader  = ShaderCompiler::Compile(compiler, shaderInfo);
-        ASSERT_FALSE(compiledShader.SPIRVBinary.empty());
-        ASSERT_EQ(compiledShader.resources.pushConstants.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).name, "PushConstants");
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).type.baseType, OCRA::ShaderCompiler::ShaderResourceBaseType::Struct);
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).type.structure->members.at(0).first, "viewMatrix");
-        ASSERT_EQ(compiledShader.resources.stageInputs.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetStageInputs().at(0).name, "vPosition");
-    }
-    {
-        shaderInfo.targetAPI = ShaderCompiler::TargetAPI::Vulkan;
-        auto compiledShader  = ShaderCompiler::Compile(compiler, shaderInfo);
-        ASSERT_FALSE(compiledShader.SPIRVBinary.empty());
-        ASSERT_EQ(compiledShader.resources.pushConstants.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).name, "PushConstants");
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).type.baseType, OCRA::ShaderCompiler::ShaderResourceBaseType::Struct);
-        ASSERT_EQ(compiledShader.resources.GetPushConstants().at(0).type.structure->members.at(0).first, "viewMatrix");
-        ASSERT_EQ(compiledShader.resources.stageInputs.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetStageInputs().at(0).name, "vPosition");
-    }
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::OpenGL;
+    VerifyPushConstant(ShaderCompiler::Compile(compiler, shaderInfo));
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::Vulkan;
+    VerifyPushConstant(ShaderCompiler::Compile(compiler, shaderInfo));
 }
 
 TEST(ShaderCompiler, InstanceIndex)
@@ -205,6 +275,19 @@ TEST(ShaderCompiler, MultipleSet)
     }
 }
 
+void VerifyUniformLocation(ShaderCompiler::CompiledShader& a_CompiledShader)
+{
+    ASSERT_FALSE(a_CompiledShader.SPIRVBinary.empty());
+    ASSERT_EQ(a_CompiledShader.stageInputs.size(), 1);
+    ASSERT_EQ(a_CompiledShader.GetStageInputs().at(0).name, "vPosition");
+    ASSERT_EQ(a_CompiledShader.sampledImages.size(), 1);
+    ASSERT_EQ(a_CompiledShader.GetSampledImages().at(0).name, "uTexture");
+    ASSERT_EQ(a_CompiledShader.uniformBuffers.size(), 1);
+    ASSERT_EQ(a_CompiledShader.GetUniformBuffers().at(0).name, "uCamera");
+    ASSERT_EQ(a_CompiledShader.GetUniformBuffers().at(0).type.baseType, OCRA::ShaderCompiler::ShaderResourceBaseType::Struct);
+    ASSERT_EQ(a_CompiledShader.GetUniformBuffers().at(0).type.structure->members.at(0).name, "viewMatrix");
+}
+
 TEST(ShaderCompiler, UniformLocation)
 {
     auto compiler = ShaderCompiler::Create();
@@ -225,18 +308,8 @@ TEST(ShaderCompiler, UniformLocation)
             "   uniformLocation();                                      \n"
             "}                                                          \n"
     };
-    {
-        shaderInfo.targetAPI = ShaderCompiler::TargetAPI::OpenGL;
-        auto compiledShader  = ShaderCompiler::Compile(compiler, shaderInfo);
-        ASSERT_FALSE(compiledShader.SPIRVBinary.empty());
-        ASSERT_EQ(compiledShader.resources.stageInputs.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetStageInputs().at(0).name, "vPosition");
-    }
-    {
-        shaderInfo.targetAPI = ShaderCompiler::TargetAPI::Vulkan;
-        auto compiledShader  = ShaderCompiler::Compile(compiler, shaderInfo);
-        ASSERT_FALSE(compiledShader.SPIRVBinary.empty());
-        ASSERT_EQ(compiledShader.resources.stageInputs.size(), 1);
-        ASSERT_EQ(compiledShader.resources.GetStageInputs().at(0).name, "vPosition");
-    }
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::OpenGL;
+    VerifyUniformLocation(ShaderCompiler::Compile(compiler, shaderInfo));
+    shaderInfo.targetAPI = ShaderCompiler::TargetAPI::Vulkan;
+    VerifyUniformLocation(ShaderCompiler::Compile(compiler, shaderInfo));
 }
